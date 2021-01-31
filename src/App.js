967cf28d0ab538ed4bf2f7b1c60528c331c77b4a
RamlahKB <ramlahkb2196@gmail.com>
2021-01-31 12:19:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Redirect, Route, Switch, withRouter} from 'react-router-dom';
+import store from './store';
+import {Provider} from 'react-redux';
+import Header from './Components/Scripts/Header';
+import Login from './Components/Scripts/Login';
+import Home from './Components/Scripts/Home';
+import Task from './Components/Scripts/Tasks';
+import User from './Components/Scripts/User';
+import NewPass from './Components/Scripts/NewPass';
+import PriRoute from './Components/PriRoute';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const Main = withRouter(({location})=>{
+        return(
+            <div>
+                { location.pathname !== '/login' && <Header/>}
+                <Switch>
+                    <Route exact path='/login' component={Login} />
+                    <PriRoute exact path='/' component={Home} />
+                    <PriRoute exact path='/task' component={Task} />
+                    <PriRoute exact path='/user' component={User} />
+                    <PriRoute exact path='/newPass' component={NewPass} />
+                </Switch>
+            </div>
+        )
+    });
+    return (
+        <div>
+            <Provider store={store}>
+                <Router>
+                    <Main />
+                </Router>
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
